053feb73c66293c6e8e0fb7eb4b0a8037532e51a
Tyler Weitler <tweitler@gmail.com>
2023-01-28 12:20:29 -0500

    Implemented reset functionality
    Cleaned up some code and moved some things into functions for better readability

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ function playRound(playerSelection, computerSelection){
     //Bool to check if player won a point or not it is undefined if there is tie
     let playerWin;
 
-    console.log("Player selected " + playerSelection);
-    console.log("Computer selected " + computerSelection);
+    playSelection.innerText = ("Player selected " + playerSelection);
+    compSelection.innerText = ("Computer selected " + computerSelection);
 
     //Check if choice is the same
     if (playerSelection == computerSelection){
@@ -73,17 +73,7 @@ function playRound(playerSelection, computerSelection){
 
 function game(playerSelection)
 {
-    if (playerScore == 5|| computerScore == 5){
-        if (playerScore > computerScore){
-            console.log("Player wins the game!");
-            return;
-        }
-        else
-        {
-            console.log("Computer wins the game!");
-            return;
-        }
-    }
+
     //Get new values for each round
     const computerSelection = getComputerChoice();
     //Store the result of playerWin so we can give points
@@ -94,27 +84,72 @@ function game(playerSelection)
     else if (roundResult == false){
         computerScore ++;
     }
-    // console.log(`Player Score: ${playerScore}`);
-    // console.log(`Computer score: ${computerScore}`);
+
     pScore.innerText = (`Player Score: ${playerScore}`);
-    cScore.innerText =(`Computer score: ${computerScore}`);
+    cScore.innerText = (`Computer score: ${computerScore}`);
+
+    if (playerScore == 5|| computerScore == 5){
+        if (playerScore > computerScore){
+            winner.innerText = ("Player wins the game!");
+            gameEnd()
+            return;
+        }
+        else
+        {
+            winner.innerText = ("Computer wins the game!");
+            gameEnd()
+            return;
+        }
+    }
+}
+
+function gameEnd(){
+    rockBtn.disabled = true;
+    scissorBtn.disabled = true;
+    paperBtn.disabled = true;
+    resetBtn.style.visibility = 'visible';
+}
+
+function resetGame(){
+    pScore.innerText = (`Player Score: 0`);
+    cScore.innerText = (`Computer score: 0`);
+    playerScore = 0;
+    computerScore = 0;
+    resetBtn.style.visibility = 'hidden';
+    winner.innerText = 'Rock Paper Scissors';
+    playSelection.innerText = '';
+    compSelection.innerText = '';
+    roundResult.innerText = '';
+    rockBtn.disabled = false;
+    scissorBtn.disabled = false;
+    paperBtn.disabled = false;
 }
 
 const rockBtn = document.getElementById('rbtn');
-const papperBtn = document.getElementById('pbtn');
+const paperBtn = document.getElementById('pbtn');
 const scissorBtn = document.getElementById('sbtn');
 let roundResult = document.getElementById('roundResult');
 let pScore = document.getElementById('pScore');
 let cScore = document.getElementById('cScore');
+let resetBtn = document.getElementById('resetBtn');
+let winner = document.getElementById('winner');
+let compSelection = document.getElementById('compSelection');
+let playSelection = document.getElementById('playerSelection');
+
+resetBtn.style.visibility = 'hidden';
 
 rockBtn.addEventListener('click', () =>{
     game('rock')
 }); 
 
-papperBtn.addEventListener('click', () =>{
+paperBtn.addEventListener('click', () =>{
     game('paper')
 }); 
 
 scissorBtn.addEventListener('click', () =>{
     game('scissors')
 }); 
+
+resetBtn.addEventListener('click', () => {
+    resetGame();
+});
